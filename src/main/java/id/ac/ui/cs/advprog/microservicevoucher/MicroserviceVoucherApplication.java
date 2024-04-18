c699c145705f1f8e25d3b1339fec0ada03865072
AryaDK153 <arya.daniswara@ui.ac.id>
2024-04-18 23:28:03 +0700

    reinitialize springboot... again

---
+++ src/main/java/id/ac/ui/cs/advprog/microservicevoucher/MicroserviceVoucherApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.microservicevoucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceVoucherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceVoucherApplication.class, args);
+	}
+
+}
