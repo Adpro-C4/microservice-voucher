38b80b5c2835dc14ca97a9d6e9f07feae5e9f2f8
AryaDK153 <arya.daniswara@ui.ac.id>
2024-04-21 14:13:53 +0700

    [REFACTOR] Reimplement Voucher model constructor and preparation for database usage

--- src/main/java/id/ac/ui/cs/advprog/microservicevoucher/VoucherModule/model/Voucher.java
+++ src/main/java/id/ac/ui/cs/advprog/microservicevoucher/VoucherModule/model/Voucher.java
@@ -1,25 +1,38 @@
 package id.ac.ui.cs.advprog.microservicevoucher.VoucherModule.model;
 
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Builder
 @Getter
+@Entity
 public class Voucher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long voucherId;
+
+    @Column(name = "voucher_name", nullable = false)
     private String voucherName;
+
     @Setter
+    @Column(name = "voucher_desc")
     private String voucherDesc;
+
+    @Column(name = "voucher_discount", nullable = false)
     private Double voucherDiscount;
+
+    @Column(name = "voucher_quota", nullable = false)
     private Integer voucherQuota;
 
     public Voucher(Long voucherId, String voucherName, String voucherDesc, Double voucherDiscount, Integer voucherQuota) {
         this.voucherId = voucherId;
-        this.voucherName = voucherName;
+        setVoucherName(voucherName);
         this.voucherDesc = voucherDesc;
-        this.voucherDiscount = voucherDiscount;
-        this.voucherQuota = voucherQuota;
+        setVoucherDiscount(voucherDiscount);
+        setVoucherQuota(voucherQuota);
     }
 
     public void setVoucherName(String newVoucherName) {
