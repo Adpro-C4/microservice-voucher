7f71205e06dc2f62a129a58d52a6126689f7787d
AryaDK153 <arya.daniswara@ui.ac.id>
2024-05-22 15:05:31 +0700

    [REFACTOR] modified constructor

--- src/main/java/id/ac/ui/cs/advprog/microservicevoucher/VoucherModule/model/Notification.java
+++ src/main/java/id/ac/ui/cs/advprog/microservicevoucher/VoucherModule/model/Notification.java
@@ -6,20 +6,22 @@ import lombok.Getter;
 @Getter
 public class Notification {
     String voucherName;
-    String voucherDiscount;
-    String voucherQuota;
-    NotificationStatus status;
+    Double voucherDiscount;
+    Integer voucherQuota;
+    String status;
+    String message;
 
-    public Notification(String name, Double discount, Integer quota, NotificationStatus status) {
+    public Notification(String name, Double discount, Integer quota, String status) {
         this.voucherName = name;
-        this.voucherDiscount = String.format("%.2f%%", (discount*100));
-        this.voucherQuota = quota.toString();
+        this.voucherDiscount = discount;
+        this.voucherQuota = quota;
         setStatus(status);
     }
 
-    public void setStatus(NotificationStatus status) {
-        if (NotificationStatus.contains(status.name())) {
+    public void setStatus(String status) {
+        if (NotificationStatus.contains(status)) {
             this.status = status;
+            this.message = String.format("%sVoucher Name: %s\nValue: %.2f%% off\nUsage Quota: %d", NotificationStatus.valueOf(status).getMsg(), this.voucherName, (this.voucherDiscount*100), this.voucherQuota);
         } else {
             throw new IllegalArgumentException();
         }
