e8f29230ba8aa211ae39750f2e9c2d02e00c65cb
AryaDK153 <arya.daniswara@ui.ac.id>
2024-05-23 22:24:11 +0700

    [REFACTOR] trial fix for DTONotification

--- src/main/java/id/ac/ui/cs/advprog/microservicevoucher/ReceiverModule/model/dto/DTONotification.java
+++ src/main/java/id/ac/ui/cs/advprog/microservicevoucher/ReceiverModule/model/dto/DTONotification.java
@@ -1,16 +1,13 @@
 package id.ac.ui.cs.advprog.microservicevoucher.ReceiverModule.model.dto;
 
-import enums.NotificationStatus;
 import lombok.Getter;
 import lombok.Setter;
 
+@Setter
 @Getter
 public class DTONotification {
-    @Setter
     String voucherName;
-    @Setter
     Double voucherDiscount;
-    @Setter
     Integer voucherQuota;
     String status;
     String message;
@@ -19,21 +16,7 @@ public class DTONotification {
         this.voucherName = voucherName;
         this.voucherDiscount = voucherDiscount;
         this.voucherQuota = voucherQuota;
-        setStatus(status);
-        setMessage(message);
-    }
-
-    public void setStatus(String status) {
-        if (!NotificationStatus.contains(status)) {throw new IllegalArgumentException();}
         this.status = status;
-    }
-
-    public void setMessage(String message) {
-        String expectedMessage = String.format("%sVoucher Name: %s\nValue: %.2f%% off\nUsage Quota: %d", NotificationStatus.valueOf(this.status).getMsg(), this.voucherName, (this.voucherDiscount*100), this.voucherQuota);
-        if (message.equals(expectedMessage)) {
-            this.message = message;
-        } else {
-            throw new IllegalArgumentException();
-        }
+        this.message = message;
     }
 }
