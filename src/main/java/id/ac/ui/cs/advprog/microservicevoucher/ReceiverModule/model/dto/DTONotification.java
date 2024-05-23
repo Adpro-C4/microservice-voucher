e03f7875083b4a7943f5473b9b6972ce549cf531
AryaDK153 <arya.daniswara@ui.ac.id>
2024-05-23 15:59:17 +0700

    [REFACTOR] modified DTONotification based on tests

--- src/main/java/id/ac/ui/cs/advprog/microservicevoucher/ReceiverModule/model/dto/DTONotification.java
+++ src/main/java/id/ac/ui/cs/advprog/microservicevoucher/ReceiverModule/model/dto/DTONotification.java
@@ -1,16 +1,31 @@
 package id.ac.ui.cs.advprog.microservicevoucher.ReceiverModule.model.dto;
 
-import lombok.Builder;
+import enums.NotificationStatus;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
-@Builder
 public class DTONotification {
+    @Setter
     String voucherName;
-    String voucherDiscount;
-    String voucherQuota;
+    @Setter
+    Double voucherDiscount;
+    @Setter
+    Integer voucherQuota;
     String status;
     String message;
+
+    public void setStatus(String status) {
+        if (!NotificationStatus.contains(status)) {throw new IllegalArgumentException();}
+        this.status = status;
+    }
+
+    public void setMessage(String message) {
+        String expectedMessage = String.format("%sVoucher Name: %s\nValue: %.2f%% off\nUsage Quota: %d", NotificationStatus.valueOf(this.status).getMsg(), this.voucherName, (this.voucherDiscount*100), this.voucherQuota);
+        if (message.equals(expectedMessage)) {
+            this.message = message;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
 }
