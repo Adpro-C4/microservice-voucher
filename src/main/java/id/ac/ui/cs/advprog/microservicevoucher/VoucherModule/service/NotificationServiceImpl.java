1c4c2fbd142c407d0a4600cf81a7c066736e11c4
AryaDK153 <arya.daniswara@ui.ac.id>
2024-05-25 10:19:18 +0700

    [REFACTOR] cleaning up code

--- src/main/java/id/ac/ui/cs/advprog/microservicevoucher/VoucherModule/service/NotificationServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/microservicevoucher/VoucherModule/service/NotificationServiceImpl.java
@@ -5,6 +5,7 @@ import id.ac.ui.cs.advprog.microservicevoucher.VoucherModule.model.Voucher;
 import id.ac.ui.cs.advprog.microservicevoucher.VoucherModule.model.dto.DTOCustomer;
 import id.ac.ui.cs.advprog.microservicevoucher.VoucherModule.repository.dto.DTOCustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -19,6 +20,9 @@ public class NotificationServiceImpl implements NotificationService{
 
     @Autowired
     RestTemplate restTemplate;
+
+    @Value("${notification.update.uri}")
+    private String notificationUpdateUri;
 
     @Override
     public DTOCustomer acceptNotification(DTOCustomer customer) {
@@ -44,7 +48,7 @@ public class NotificationServiceImpl implements NotificationService{
         List<DTOCustomer> customers = customerRepository.findAll();
         for (DTOCustomer customer:customers) {
             try {
-                String uri = String.format("http://34.143.184.254/customer-notification/update?username=%s", customer.getUsername());
+                String uri = notificationUpdateUri+customer.getUsername();
                 CompletableFuture<Void> future = customer.update(payload, uri, restTemplate);
                 future.thenRun(() -> System.out.printf("Update completed for customer %s%n", customer.getUsername()));
             } catch (RestClientException ignored) {
