dabbeca6a5213ceec2c9dbed339d3fe0a92169f1
AryaDK153 <arya.daniswara@ui.ac.id>
2024-05-25 16:53:13 +0700

    [REFACTOR] test prometheus

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ sonarqube {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.postgresql:postgresql")
 
